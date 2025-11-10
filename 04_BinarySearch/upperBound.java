3a60ecf96cab201337aad134dda35765bd344aee
Abhay Chaudhary <abhaychaudhary2023@gmail.com>
2025-11-10 14:03:12 +0530

    5 questions done

---
+++ 04_BinarySearch/upperBound.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int upperBound(int[] nums, int x) {
+ int left = 0;
+    int right = nums.length - 1;
+    int ans = nums.length;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (nums[mid] > x) {  
+            ans = mid;       
+            right = mid - 1;  
+        } else {
+            left = mid + 1;   
+        }
+    }
+    
+    return ans;
+    }
+}
