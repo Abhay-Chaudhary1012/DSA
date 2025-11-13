5bab06a4a904899b09f092e9f7fc71637cf99605
Abhay Chaudhary <abhaychaudhary2023@gmail.com>
2025-11-13 23:51:16 +0530

    All questions till now

--- 04_BinarySearch/searchInSortedArr2.java
+++ 04_BinarySearch/searchInSortedArr2.java
@@ -8,34 +8,34 @@ You must decrease the overall operation steps as much as possible */
 
 class Solution {
     public boolean search(int[] nums, int target) {
-     int left = 0;
-     int right = nums.length -1;
+        int left = 0;
+        int right = nums.length - 1;
 
-     while(left <= right){
-        int mid = left + (right - left)/2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        if(nums[mid] == target){
-            return true;
-        }
-         if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
                 left++;
                 right--;
                 continue;
             }
-        if(nums[left]<= nums[mid]){
-            if(target >= nums[left] && target < nums[mid]){
-                right = mid -1;
-            }else{
-                left = mid +1;
-            }
-        }else{
-            if(target > nums[mid] && target <= nums[right]){
-                left = mid +1;
-            }else{
-                right = mid -1;
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-     }   
-     return false;
+        return false;
     }
 }
