f23cfa87efea5a3fe1607c9af7102f0e09e35d61
Abhay Chaudhary <abhaychaudhary2023@gmail.com>
2025-11-15 17:17:51 +0530

    All questions till now

---
+++ 04_BinarySearch/indexOfRoatation.java
@@ -0,0 +1,18 @@
+/*Given an integer array nums of size n, sorted in ascending order with distinct values. The array has been right rotated an unknown number of times, between 0 and n-1 (including). Determine the number of rotations performed on the array.*/
+public int findRotationCount(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+} 
