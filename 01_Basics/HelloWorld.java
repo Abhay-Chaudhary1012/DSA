1c163993e4770d8de28b9d7359b1c28347913f17
Abhay Chaudhary <abhaychaudhary2023@gmail.com>
2025-11-22 12:41:56 +0530

    All questions till now

--- 01_Basics/HelloWorld.java
+++ 01_Basics/HelloWorld.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }  
 }
-=======
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }  
-}
->>>>>>> aabe6538746c7783f241b156bff20dbe3a167194
