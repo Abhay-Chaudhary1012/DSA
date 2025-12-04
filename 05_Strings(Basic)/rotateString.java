9dd0f5b53f6baaa3b377e2e44a99d4469a38c901
Abhay Chaudhary <abhaychaudhary2023@gmail.com>
2025-12-04 23:01:03 +0530

    All questions till now

---
+++ 05_Strings(Basic)/rotateString.java
@@ -0,0 +1,7 @@
+public class rotateString {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        String temp = s + s;
+        return temp.contains(goal);
+    }
+}
